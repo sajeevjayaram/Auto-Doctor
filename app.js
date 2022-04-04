6719234db424388f2ae131e0b5c53e2ddc43e5ac
iamsajith <sajithj03@gmail.com>
2022-04-04 21:21:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+document.getElementById('sign-in-form').addEventListener("submit",(event)=>{
+  event.preventDefault()
+});
+document.getElementById('sign-up-form').addEventListener("submit",(event)=>{
+  event.preventDefault()
+});
